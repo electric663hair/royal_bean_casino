4cd0a705175277646228db9ba387269d6fe14bc8
electric663hair <electric663hair@gmail.com>
2024-10-31 13:11:09 +0100

    Updated code

--- log-bean/script.js
+++ log-bean/script.js
@@ -1,16 +1,23 @@
+function initClient() {
+    gapi.load('auth2', function() {
+        gapi.auth2.init({
+            client_id: '607568017086-sjl3pgll8kc2mq4u09vdv9qja27lsmla.apps.googleusercontent.com'
+        });
+    });
+}
+
 function onSignIn(googleUser) {
     var profile = googleUser.getBasicProfile();
-    document.querySelector("#name").innerHTML = profile.getName();
-    document.querySelector("#email").innerHTML = profile.getEmail();
-    alert('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    alert('Image URL: ' + profile.getImageUrl());
+    document.querySelector("#name").innerText = profile.getName();
+    document.querySelector("#email").innerText = profile.getEmail();
+    document.querySelector("#image").innerHTML = `<img src="${profile.getImageUrl()}" alt="Profile Image">`;
     document.querySelector(".data").style.display = "block";
     document.querySelector(".g-signin2").style.display = "none";
 }
 
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+    auth2.signOut().then(function() {
         alert('User signed out.');
         document.querySelector(".g-signin2").style.display = "block";
         document.querySelector(".data").style.display = "none";
