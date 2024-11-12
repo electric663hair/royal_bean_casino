718d00912497c26b20f4e5dbdaa5a4a4d97682a1
GuriGris <149690987+GuriGris@users.noreply.github.com>
2024-11-12 20:25:03 +0100

    Laget responsive for mobil. Må fikse for andre skjermstørrelser fortsatt

--- main.js
+++ main.js
@@ -2,6 +2,5 @@ const dBody = document.body;
 const backButton = document.createElement("a");
 backButton.classList.add("backButton")
 backButton.classList.add("ssNone")
-backButton.innerText = "Tilbake";
 backButton.href = "..\\#play";
 dBody.insertBefore(backButton, dBody.firstChild);
