66cc43345a81ee91be847bae08e3ba7420f4b78b
ApikGamBom <east123beast@gmail.com>
2024-12-03 23:44:34 +0100

    blackjack fix

--- Games/Blackjack/blackjack.js
+++ Games/Blackjack/blackjack.js
@@ -7,6 +7,7 @@ const standButton = document.getElementById("stand")
 const balanceText = document.getElementById("balanceText")
 const playerDiv = document.getElementById("playerCards")
 const dealerDiv = document.getElementById("dealerCards")
+const input = document.getElementById("betInput")
 
 let playerImgElements = playerDiv.querySelectorAll("img")
 let dealerImgElements = dealerDiv.querySelectorAll("img")
@@ -28,22 +29,20 @@ async function checkForm(){
     playerHandSum = 0;
     dealerHandSum = 0;
 
-
-    
     playerImgElements.forEach(img => img.remove())
     dealerImgElements.forEach(img => img.remove())
 
-    betAmount = parseFloat(document.getElementById("betInput").value);
+    betAmount = parseFloat(input.value);
     if (!isNaN(betAmount) && betAmount <= balance && betAmount > 0) {
         balance -= betAmount
         balanceText.innerHTML = `Balance: ${balance}`
         playerHand = [], dealerHand = []
         let returnValue = await play();
+        text.classList.remove("none")
         if (returnValue === "tie") {
             text.innerHTML = "Push";
             balance += betAmount;
-        }
-        if (returnValue == "win") {
+        } else if (returnValue == "won") {
             text.innerHTML = "You won"
             balance += betAmount * 2
         } else if (returnValue == "lost") {
@@ -55,8 +54,7 @@ async function checkForm(){
         standButton.classList.add("none")
         hitButton.classList.add("none")
         betButton.classList.remove("none")
-    } else {
-        console.error("The desired value is not allowed")
+        input.classList.remove("none")
     }
 }
 
@@ -64,6 +62,7 @@ const betButton = document.getElementById("start")
 betButton.addEventListener("click", function() {
     checkForm();
     betButton.classList.add("none")
+    text.classList.add("none")
 });
 
 function identify(IdentifyCard) {
@@ -78,25 +77,26 @@ function refreshDeck() {
     return deck;
 }
 
-function drawCard(hand, containerId, isHidden) {
+function drawCard(hand, containerId, isHidden, isStand) {
     let index = Math.floor(Math.random() * deck.length)
     let card = deck[index]
     deck.splice(index, 1)
     let integer = identify(card)
     hand.push([ card, integer, isHidden ])
 
-    displayCards(hand, containerId)
+
+    displayCards(hand, containerId, isStand)
     
 }
 
-function displayCards(hand, containerId) {
+function displayCards(hand, containerId, isStand) {
 
-    playerImgElements.forEach(img => img.remove())
-    dealerImgElements.forEach(img => img.remove())
+    const container = document.getElementById(containerId);
+    container.innerHTML = '';
 
-    for (let i = 1; i < hand.length; i++) {
+    for (let i = 0; i < hand.length; i++) {
         const img = document.createElement("img");
-        if (hand[i][2]) {
+        if (hand[i][2] && !isStand) {
             img.src = `../../resources/cards_png/blue_back.png`;
         } else {
             img.src = `../../resources/cards_png/${hand[i][0]}.png`;
@@ -105,6 +105,8 @@ function displayCards(hand, containerId) {
 
         document.getElementById(containerId).appendChild(img);
     }
+
+
 }
 
 function sum(hand, textId, addFirstCard) {
@@ -144,7 +146,11 @@ function sum(hand, textId, addFirstCard) {
     if (sum <= 11 && ace) {
         sum += 10
     }
-    document.getElementById(textId).innerHTML = `${textId}: ${sum}`
+    if (textId == "playerText") {
+        document.getElementById(textId).innerHTML = `Player Hand: ${sum}`
+    } else {
+        document.getElementById(textId).innerHTML = `Dealer Hand: ${sum}`
+    }
     return sum
 }
 
@@ -154,8 +160,8 @@ let dealerHandSum = 0;
 
 document.getElementById("hit").addEventListener("click", function () {
     if (playerHandSum < 21) {
-        drawCard(playerHand, "playerCards"), false;
-        playerHandSum = sum(playerHand, "playerSum", true);
+        drawCard(playerHand, "playerCards", false, false);
+        playerHandSum = sum(playerHand, "playerText", true);
         if (playerHandSum > 21) {
             currentPromiseResolver(false);
         }
@@ -165,8 +171,8 @@ document.getElementById("hit").addEventListener("click", function () {
 document.getElementById("stand").addEventListener("click", function () {
     hitButton.classList.add("none");
     while (dealerHandSum < 17) {
-        drawCard(dealerHand, "dealerCards");
-        dealerHandSum = sum(dealerHand, "dealerSum", true);
+        drawCard(dealerHand, "dealerCards", false, true);
+        dealerHandSum = sum(dealerHand, "dealerText", true);
     }
     if (dealerHandSum > 21 || playerHandSum > dealerHandSum) {
         currentPromiseResolver("won");
@@ -186,18 +192,19 @@ function play() {
     resetGame()
     return new Promise((resolve) => {
         currentPromiseResolver = resolve;
+        input.classList.add("none")
         hitButton.classList.remove("none");
         standButton.classList.remove("none");
 
         deck = refreshDeck();
 
-        drawCard(playerHand, "playerCards", false);
-        drawCard(playerHand, "playerCards", false);
+        drawCard(playerHand, "playerCards", false, false);
+        drawCard(playerHand, "playerCards", false, false);
 
-        drawCard(dealerHand, "dealerCards", true);
-        drawCard(dealerHand, "dealerCards", false);
+        drawCard(dealerHand, "dealerCards", true), false;
+        drawCard(dealerHand, "dealerCards", false, false);
 
-        playerHandSum = sum(playerHand, "playerSum", true);
-        dealerHandSum = sum(dealerHand, "dealerSum", false);
+        playerHandSum = sum(playerHand, "playerText", true);
+        dealerHandSum = sum(dealerHand, "dealerText", false);
     });
 }
