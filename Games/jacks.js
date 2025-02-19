1e658840f1fd4fd1f680c86bd8ea12749c2599c5
Aron Bjorke <162693902+electric663hair@users.noreply.github.com>
2025-02-19 20:12:04 +0100

    Testing audio button click

--- Games/jacks.js
+++ Games/jacks.js
@@ -275,6 +275,7 @@ function toggleSoundIcon() {
 }
 function soundToggle() {
     game.balance += 10000;
+    alert("test")
     saveBalance(game.balance)
     soundToggleSound.play();
     soundToggleVar = !soundToggleVar;
